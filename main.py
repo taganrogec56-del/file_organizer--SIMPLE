dba3d9c4469390448e17d4ab1df0fba6c4a82b07
alex_fill <taganrogec56@gmail.com>
2026-09-07 07:36:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+FILE_CATEGORIES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".webp", ".bmp", ".svg"],
+    "Documents": [
+        ".txt",
+        ".pdf",
+        ".doc",
+        ".docx",
+        ".xls",
+        ".xlsx",
+        ".ppt",
+        ".pptx",
+        ".csv",
+    ],
+    "Archives": [".zip", ".rar", ".7z", ".tar", ".gz"],
+    "Music": [".mp3", ".wav", ".flac", ".aac", ".ogg"],
+    "Videos": [".mp4", ".avi", ".mkv", ".mov", ".webm"],
+    "Code": [".py", ".js", ".html", ".css", ".json", ".xml", ".md"],
+}
+
+def main():
+    while True:
+        path_folder = input('Введите путь к папке: ').strip()
+
+
+
+if __name__ == '__main__':
+    main()
