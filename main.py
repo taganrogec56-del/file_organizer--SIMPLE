877cef30f31e4636030e47560eb2b82f7bba209c
alex_fill <taganrogec56@gmail.com>
2026-09-07 08:48:42 +0300

    feat: add folder error handling and file category detection

--- main.py
+++ main.py
@@ -21,14 +21,26 @@ FILE_CATEGORIES = {
 
 
 def get_folder_path(path: str) -> Path | None:
-    path_folder = Path(path).resolve()
-    if not path_folder.exists():
-        print('Ошибка: указанная папка не существует')
-        return
-    if not path_folder.is_dir():
-        print('Ошибка: указанный путь не является папкой')
-        return
-    return path_folder
+    try:
+        path_folder = Path(path).resolve()
+        if not path_folder.exists():
+            print('Ошибка: указанная папка не существует')
+            return None
+        if not path_folder.is_dir():
+            print('Ошибка: указанный путь не является папкой')
+            return None
+        return path_folder
+    except OSError as error:
+        print(f'Системная ошибка: {error}')
+        return None
+
+
+def get_category(suffix: str) -> str:
+    suffix = suffix.lower()
+    for category, exts in FILE_CATEGORIES.items():
+        if suffix in exts:
+            return category
+    return 'Other'
 
 
 def main():
@@ -37,6 +49,15 @@ def main():
         path_folder = get_folder_path(path)
         if path_folder is not None:
             print('Путь принят')
+            try:
+                files_from_folder = path_folder.iterdir()
+                for file_path in files_from_folder:
+                    if file_path.is_file():
+                        category = get_category(file_path.suffix)
+                        print(category)
+            except OSError as error:
+                print(f'Системная ошибка: {error}')
+                continue
             break
 
 
