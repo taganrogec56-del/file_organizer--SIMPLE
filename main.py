18ecd62b47abb29392f25beef656c3243c546147
alex_fill <taganrogec56@gmail.com>
2026-09-07 08:01:02 +0300

    feat: add file categories and folder path validation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 FILE_CATEGORIES = {
     "Images": [".jpg", ".jpeg", ".png", ".gif", ".webp", ".bmp", ".svg"],
     "Documents": [
@@ -17,10 +19,25 @@ FILE_CATEGORIES = {
     "Code": [".py", ".js", ".html", ".css", ".json", ".xml", ".md"],
 }
 
+
+def get_folder_path(path: str) -> Path | None:
+    path_folder = Path(path).resolve()
+    if not path_folder.exists():
+        print('Ошибка: указанная папка не существует')
+        return
+    if not path_folder.is_dir():
+        print('Ошибка: указанный путь не является папкой')
+        return
+    return path_folder
+
+
 def main():
     while True:
-        path_folder = input('Введите путь к папке: ').strip()
-
+        path = input('Введите путь к папке: ').strip()
+        path_folder = get_folder_path(path)
+        if path_folder is not None:
+            print('Путь принят')
+            break
 
 
 if __name__ == '__main__':
